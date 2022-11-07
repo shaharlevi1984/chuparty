ba2cd6d2d76907927052ed31360224e4ddce6f6a
Shahar Levi <shaharLe88@gmail.com>
2022-11-07 21:41:39 +0200

    create home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="left-nav">
+          <div className="logo-container">
+            <div className="logo-text"><a href="#">Chuparty</a></div>
+            <div className="logo-img"><img src="./assets/logo.png" alt="icon" id="logo-img"/></div>
+          </div>
+        </div>
+      </nav>
+
+      <div className="header">
+        <header>ברוכים הבאים ל- !ChuParty</header>
+      </div>
+
+      <section className="info">
+      <div className="details">
+      המערכת המספקת את היכולת לתרגל את הקורסים שלכם, <br/>
+            ולפתור מבחנים בצורה נוחה <br/>
+            <button class="login">Login</button>
+      </div>
+      </section>
+
+      <div class="banner">
+        <img src="./assets/chuparty_banner.png" alt="banner"/>
+      </div>
     </div>
   );
 }
